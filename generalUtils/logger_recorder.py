1ec93d8ef50ad4a9f8c9111ab13bb078574e2149
timjolson <tim.lsn@gmail.com>
2019-05-18 14:43:19 -0500

    Add MultiLineFormatter to logger_recorder

--- generalUtils/logger_recorder.py
+++ generalUtils/logger_recorder.py
@@ -219,4 +219,24 @@ class LogAndRecord():
         pass
 
 
-__all__ = ['Logger', 'Recorder', 'LogAndRecord']
+
+class MultilineFormatter(logging.Formatter):
+    """
+    Isnpired by https://stackoverflow.com/a/45217732
+    """
+
+    def format(self, record: logging.LogRecord):
+        save_msg = record.msg
+        if not isinstance(save_msg, str):
+            save_msg = str(save_msg)
+        output = ""
+        for line in save_msg.splitlines():
+            record.msg = line
+            output += super().format(record) + "\n"
+        output = output[:-1]
+        record.msg = save_msg
+        record.message = output
+        return output
+
+
+__all__ = ['Logger', 'Recorder', 'LogAndRecord', 'MultilineFormatter']
