955a4f4eaef8cafd99ec82b17af75ea2fafd8889
timjolson <tim.lsn@gmail.com>
2019-07-18 17:54:52 -0500

    Refactored data processing for cleanliness and explicitness. Added __setitem__ support. Expanded tests.

--- generalUtils/datastream.py
+++ generalUtils/datastream.py
@@ -8,12 +8,12 @@ _ascii_fields = string.ascii_letters[23:26] + string.ascii_letters[0:23]
 
 
 class DataStream():
-    def __init__(self, data=None, record_to_file=None, pause=False, file_format='csv'):
-        self.pause = pause
+    def __init__(self, data=None, record_to_file=None, pause=None, file_format='csv'):
         self._keys = ()
         self._file_inited = False
         self._recorder = lambda *x, **y: None
         self.file_format = file_format
+        self.pause = pause if (pause is not None) else (record_to_file is None)
         self.set_record_file(record_to_file, format=file_format)
 
         if data is None:
@@ -26,34 +26,20 @@ class DataStream():
             raise ValueError("No point provided")
         elif not data and kwargs:
             data = kwargs
-
-        if len(data) == 1:
+        elif len(data) == 1:
             data = data[0]
 
         data, names = self._process_data(data)
-        self.points = np.concatenate([self.points, data], 1)
-        if not self.pause and self._recorder:
-            for p in data:
-                self._recorder(self._format_point(p))
-
-    def add_point(self, *data, **kwargs):
-        if not data and not kwargs:
-            raise ValueError("No point provided")
-        elif not data and kwargs:
-            data = kwargs
-
-        if len(data) == 1:
-            data = data[0]
-
-        print('data', data, type(data))
-        data, names = self._process_data(data)
-        print('data', data, type(data), data.shape, 'names', names, 'points', self.points.shape)
         oldlength = self.points.size
         if oldlength == 0:
             self.points = data.copy()
         else:
             self.points = np.concatenate([self.points, data], 1)
-        self._record_point(data)
+        if not self.pause and self._recorder:
+            if not self._file_inited:
+                self._init_file()
+            for p in data.T:
+                self._recorder(self._format_point(p))
 
     def start_recording(self):
         self.pause = False
@@ -95,7 +81,6 @@ class DataStream():
             yield k
 
     def _format_point(self, point):
-        print('format', point)
         point = point.flatten()
         if self.file_format == 'csv':
             msg = ','.join(str(v) for v in point)
@@ -125,82 +110,63 @@ class DataStream():
                 self._init_file()
             self._recorder(self._format_point(point))
 
-    @staticmethod
-    def _check_list_tuple(things):
-        print('check list tuple', type(things), things)
-        if len(things) == 0:
-            return
-
-        sample = things[0]
-        if isinstance(sample, (int, float, np.number)):
-            return
-        # if len(sample) > 1: # and hasattr(sample[0], '__iter__'):
-        #     raise TypeError('Data arrays must not be multi-dimensional.')
-
-        if not all(len(v) == len(sample) for v in things):
-            raise ValueError('Not all arrays are the same length.')
-
-    @staticmethod
-    def _check_numpy_array(things):
-        if len(things) == 0:
-            return
-
-        sample = things[0]
-        if len(sample.shape) > 1:
-            raise TypeError('Data arrays must not be multi-dimensional.')
-
-        if not all(v.shape == sample.shape for v in things):
-            raise ValueError('Not all arrays are the same length.')
-
     def _process_data(self, data):
-        if hasattr(data, '_fields') and isinstance(data, tuple):  # namedtuple
-            names = data._fields
-            sample = data
-        elif isinstance(data, dict):
-            names, vv = [], []
-            for k, v in data.items():
-                names.append(k)
-                vv.append(v)
+        names = []
+        dt = data_type(data)
+        if dt == 'empty':
+            data = np.array([])
+        elif dt == 'listOfLists':
+            sample = data[0]
+            if hasattr(data, '_fields'):  # namedtuple
+                names = data._fields
+            elif hasattr(sample, '_fields'):  # namedtuple
+                names = sample._fields
+            else:
+                names = self._keys or [_ascii_fields[i] for i in range(len(data))]
+        elif dt == 'listOfDicts':
+            sample = data[0]
+            names = list(sample.keys())
+            vv = []
+            for k in sample.keys():  # curve names
+                pp = []  # data for curve
+                for p in data:  # each data point
+                    pp.append(p[k])  # coordinate
+                vv.append(pp)  # data for curve
             data = vv
-            sample = data[0]
-        elif isinstance(data, tuple):
-            names = [_ascii_fields[i] for i in range(len(data))]
-            sample = data[0]
-        elif isinstance(data, (np.ndarray, list)):
-            if isinstance(data, list):
-                data = np.array(data).T
-            try:
-                names = data.dtype['names']  # structured array
-            except KeyError:
-                names = [_ascii_fields[i] for i in range(len(data))]
-            sample = data[:, 0]
-        elif isinstance(data, DataStream):
-            names = list(data.keys())
+        elif dt == 'listOfValues':
+            if hasattr(data, '_fields'):  # namedtuple
+                names = data._fields
+            else:
+                names = self._keys or [_ascii_fields[i] for i in range(len(data))]
+            data = [[d] for d in data]
+        elif dt == 'dictOfLists':
+            names, data = list(data.keys()), list(data.values())
+        elif dt == 'dictOfValues':
+            names, data = list(data.keys()), [[d] for d in data.values()]
+        elif dt == 'recarray':
+            names = data.dtype['names']
+        elif dt == 'ndarray':
+            names = self._keys or [_ascii_fields[i] for i in range(len(data))]
+        elif dt == 'DataStream':
+            names = tuple(data._keys)
             data = data.points.copy()
-            sample = data[:, 0]
         else:
-            raise NotImplementedError((type(data), data))
+            raise NotImplementedError(f"Cannot handle '{dt}' {data}")
 
-        if isinstance(sample, np.ndarray):
-            self._check_numpy_array(data)
-        elif isinstance(sample, (list, tuple)):
-            self._check_list_tuple(data)
-        elif isinstance(sample, (int, float, np.number)):
-            data = np.array(data, dtype=np.float64).T.reshape(len(names), 1)
-        else:
-            raise NotImplementedError(f"Sample incorrect type {type(sample)}")
+        if self._keys and len(self._keys) != len(data): raise ValueError(
+            f"Improper data count ({len(data)}) provided, need ({len(self._keys)})")
+
+        data = np.array(data, dtype=np.float64)
 
         if self._keys:  # we have column names
-            sdata = np.zeros_like(data)
+            sdata = np.ones_like(data)*np.nan  # sorted data
+
             for i, k in enumerate(self._keys):  # sort incoming data columns
-                try:
-                    sdata[i, :] = data[names.index(k)]
-                except ValueError as e:
-                    raise ValueError(f"{k} value not provided")
+                sdata[i, :] = data[names.index(k)][:]
             return sdata, self._keys
-        else:
+        else:  # set data column names
             self._keys = tuple(names)
-        return np.asarray(data, dtype=np.float64), tuple(names)
+            return data.copy(), tuple(names)
 
     def __getattr__(self, attr):
         try:
@@ -211,22 +177,87 @@ class DataStream():
     def __getitem__(self, items):
         if isinstance(items, str):
             if items in self._keys:
-                items = list(self.keys()).index(items)
+                items = self._keys.index(items)
+            else:
+                # force KeyError
+                a = dict()[items]
         elif isinstance(items, tuple):
-            kk = list(self.keys())
-            items = tuple((kk.index(i) if isinstance(i, str) else i) for i in items)
-        elif isinstance(items, (int, slice)):
-            pass
-        else:
-            raise NotImplementedError((type(items), items))
+            if isinstance(items[0], str):
+                # handle string indexing -> column name
+                if len(items) > 1:
+                    items = self._keys.index(items[0]), *items[1:]
+                else:
+                    items = self._keys.index(items[0]),
         return self.points.__getitem__(items)
+
+    def __setitem__(self, key, value):
+        if isinstance(key, str):
+            if key in self._keys:
+                key = self._keys.index(key)
+            else:
+                # force KeyError
+                a = dict()[key]
+        elif isinstance(key, tuple):
+            if isinstance(key[0], str):
+                # handle string indexing -> column name
+                if len(key) > 1:
+                    key = self._keys.index(key[0]), *key[1:]
+                else:
+                    key = self._keys.index(key[0]),
+        return self.points.__setitem__(key, value)
+
+    def __len__(self):
+        return self.points.__len__()
 
     def __repr__(self):
         if self.points.size > 0:
             dd = {k: self.points[i] for i, k in enumerate(self._keys)}
         else:
-            dd = {}
+            dd = {k:[] for i, k in enumerate(self._keys)}
         return f"{type(self).__name__}({dd})"
+
+
+def data_type(obj):
+    # inspired by pyqtgraph.graphicsitems.PlotDataItem
+    if hasattr(obj, '__len__') and len(obj) == 0:
+        return 'empty'
+    if isinstance(obj, DataStream):
+        return 'DataStream'
+    if isinstance(obj, dict):
+        first = obj[list(obj.keys())[0]]
+        if is_sequence(first):
+            return 'dictOfLists'
+        else:
+            return 'dictOfValues'
+    elif is_sequence(obj):
+        first = obj[0]
+
+        if (hasattr(obj, 'implements') and obj.implements('MetaArray')):
+            return 'MetaArray'
+        elif isinstance(obj, np.ndarray):
+            if obj.ndim == 1:
+                if obj.dtype.names is None:
+                    return 'listOfValues'
+                else:
+                    return 'recarray'
+            # elif obj.ndim == 2 and obj.dtype.names is None and obj.shape[1] == 2:
+            #     return 'Nx2array'
+            elif obj.ndim == 2:
+                return 'ndarray'
+            else:
+                raise Exception('array shape must be (N,) or (N,2); got %s instead' % str(obj.shape))
+        elif isinstance(first, dict):
+            return 'listOfDicts'
+        elif is_sequence(first):
+            return 'listOfLists'
+        else:
+            return 'listOfValues'
+
+
+def is_sequence(obj):
+    # inspired by pyqtgraph.graphicsitems.PlotDataItem
+    return hasattr(obj, '__iter__') or isinstance(obj, np.ndarray) or (
+                hasattr(obj, 'implements') and obj.implements('MetaArray'))
 
 
 __all__ = ['DataStream']
--- tests/test_datastream.py
+++ tests/test_datastream.py
@@ -4,25 +4,44 @@ import logging
 from collections import namedtuple
 import numpy as np
 
-from generalUtils.datastream import DataStream
+from generalUtils.datastream import DataStream, data_type
 
-logging.basicConfig(filename='logs/test_datastream.log', filemode='w', level=logging.DEBUG)
+logging.basicConfig(filename='logs/test_datastream.log', filemode='w', level=logging.INFO)
+
+# test data
+dict1 = {'x': [], 'y': []}
+dict2 = {'x': 1.0, 'y': 1.0}
+dict3 = {'x': [2.0, 3.0], 'y': [2.0, 3.0]}
+point_test = namedtuple('point_test', 'x, y')
+ntup1 = point_test([], [])
+ntup2 = point_test(1.0, 1.0)
+ntup3 = point_test([2.0, 3.0], [2.0, 3.0])
+tup1 = ([], [])
+tup2 = (1.0, 1.0)
+tup3 = ([2.0, 3.0], [2.0, 3.0])
+np1 = (np.array([]), np.array([]))
+np2 = (np.array(1.0), np.array(1.0))
+np3 = (np.array([2.0, 3.0]), np.array([2.0, 3.0]))
+VALUES1 = np.array([[],[]])
+VALUES2 = np.array([[1.0],[1.0]])
+VALUES3 = np.array([[2.0,3.0],[2.0,3.0]])
+KEYS = {'x', 'y'}
 
 
-def addpointstester(ds):
+def add_point_tester(ds):
     # using np.inf instead of None or np.nan so we can check equality between arrays
 
     logging.debug('addpoints dict')
-    ds.add_point({'x': 0, 'y': 0, 'z': 0})
+    ds.add_points({'x': 0, 'y': 0, 'z': 0})
     logging.debug('addpoints items')
-    ds.add_point(1, np.inf, 1)
+    ds.add_points(1, np.inf, 1)
     logging.debug('addpoints kwargs')
-    ds.add_point(x=2, y=1, z=np.inf)
+    ds.add_points(x=2, y=1, z=np.inf)
     logging.debug('addpoints tuple')
-    ds.add_point((3,2,2))
+    ds.add_points((3, 2, 2))
 
 
-def check_post_add_points(ds):
+def check_post_add_point(ds):
     # using np.inf instead of None or np.nan so we can check equality between arrays
 
     logging.debug('post addpoints')
@@ -35,131 +54,179 @@ def check_post_add_points(ds):
 
 def check_datastreams_equal(one, two):
     logging.debug('check_datastreams_equal')
-    logging.debug(f"Vars: {vars(one), vars(two)}")
+    logging.debug(f"keys {one._keys, two._keys}")
     logging.debug(f"points {one.points, two.points}")
 
-    for key in one.keys():
-        logging.debug(f"checking '{key}'")
-        logging.debug((one[key], two[key]))
-        assert np.all(np.equal(one[key], two[key]))
-    assert one._keys == two._keys
-    return True
+    assert set(one._keys) == set(two._keys)
+    assert np.all(one.points == two.points)
 
 
-def test_constructors():
-    logging.debug('---------------Begin test_constructors()')
+def test_base_constructor():
+    logging.debug('---------------Begin test_base_constructor()')
     ds = DataStream()
-
-    dict1 = {'x': [], 'y': []}
-    dict2 = {'x': 1.0, 'y': 1.0}
-    dict3 = {'x': [2.0, 3.0], 'y': [2.0, 3.0]}
-
-    point_test = namedtuple('point_test', 'x, y')
-    ntup1 = point_test([], [])
-    ntup2 = point_test(1.0, 1.0)
-    ntup3 = point_test([2.0, 3.0], [2.0, 3.0])
-
-    tup1 = ([], [])
-    tup2 = (1.0, 1.0)
-    tup3 = ([2.0, 3.0], [2.0, 3.0])
-
-    np1 = (np.array([]), np.array([]))
-    np2 = (np.array(1.0), np.array(1.0))
-    np3 = (np.array([2.0, 3.0]), np.array([2.0, 3.0]))
-
-    set_to_test = [(dict1, ntup1, tup1, np1),
-                   (dict2, ntup2, tup2, np2),
-                   (dict3, ntup3, tup3, np3)]
-
-    for d, nt, t, nmpy in set_to_test:
-        logging.debug(('testing', d, nt, t, nmpy))
-
-        check_datastreams_equal(DataStream(d), DataStream(nt))
-        check_datastreams_equal(DataStream(t), DataStream(nmpy))
-        check_datastreams_equal(DataStream(nt), DataStream(t))
-
-    Dict = {'x': [1., 4.], 'y': [2., 5], 'z': [3, 6]}
-    data = [(10., 11., 12), (13., 14., 15)]
-    assert check_datastreams_equal(DataStream(DataStream(Dict)), DataStream(Dict))
-    assert check_datastreams_equal(DataStream(DataStream(data)), DataStream(data))
+    assert ds.dtype == np.float64
 
 
-def test_add_point():
-    logging.debug('---------------Begin test_add_point()')
-    Dict2 = {'x': 7, 'y': 8, 'z': 9}
-    data2 = (16, 17, 18)
+def test_dataType():
+    logging.debug('---------------Begin test_dataType()')
 
-    arr = DataStream()
+    assert data_type(dict1) == 'dictOfLists'
+    assert data_type(dict2) == 'dictOfValues'
 
-    arr.add_point(Dict2)
-    assert np.all(arr['x'] == [7])
-    assert np.all(arr['y'] == [8])
-    assert np.all(arr['z'] == [9])
+    assert data_type(ntup1) == 'listOfLists'
+    assert data_type(ntup2) == 'listOfValues'
 
-    arr.add_point(data2)
-    assert np.all(arr['x'] == [7,16])
-    assert np.all(arr['y'] == [8,17])
-    assert np.all(arr['z'] == [9,18])
+    assert data_type(tup1) == 'listOfLists'
+    assert data_type(tup2) == 'listOfValues'
 
-    ds = DataStream()
-    addpointstester(ds)
-    check_post_add_points(ds)
+    assert data_type(np1) == 'listOfLists'
+    assert data_type(np2) == 'listOfLists'
+
+    assert data_type(DataStream()) == 'empty'
+
+    assert data_type(DataStream([[], [], []])) == 'DataStream'
+
+
+def test_constructor_point():
+    logging.debug('---------------Begin test_constructor_point()')
+
+    for data in [dict1, ntup1, tup1, np1]:
+        ds = DataStream(data)
+        assert data_type(ds) == 'DataStream'
+        assert np.all(ds.points == VALUES1)
+        assert set(ds.keys()) == KEYS
+
+    for data in [dict2, ntup2, tup2, np2]:
+        ds = DataStream(data)
+        assert data_type(ds) == 'DataStream'
+        assert np.all(ds.points == VALUES2)
+        assert set(ds.keys()) == KEYS
+
+
+def test_constructor_points():
+    logging.debug('---------------Begin test_constructor_points()')
+
+    for data in [dict3, ntup3, tup3, np3]:
+        ds = DataStream(data)
+        assert data_type(ds) == 'DataStream'
+        assert np.all(ds.points == VALUES3)
+        assert set(ds.keys()) == KEYS
 
 
 def test_add_points():
     logging.debug('---------------Begin test_add_points()')
-    Dict = {'x': [1., 4.], 'y': [2., 5], 'z': [3, 6]}
-    Dict2 = {'x': 7, 'y': 8, 'z': 9}
-    data = [(10., 11., 12), (13., 14., 15)]
-    data2 = (16, 17, 18)
 
-    arr = DataStream(Dict)
-    assert np.all(arr['x'] == Dict['x'])
-    assert np.all(arr['y'] == Dict['y'])
-    assert np.all(arr['z'] == Dict['z'])
+    ds = DataStream()
+    add_point_tester(ds)
+    check_post_add_point(ds)
 
-    arr.add_points(Dict2)
-    assert np.all(arr['x'] == [1.,4.,7])
-    assert np.all(arr['y'] == [2.,5,8])
-    assert np.all(arr['z'] == [3,6,9])
+    ds = DataStream()
+    ds.add_points(1,2)
+    # assert np.all(ds.points == np.array([[1],[2]]))
+    ds.add_points({'x':3,'y':4})
+    # assert np.all(ds.points == np.array([[1,3],[2,4]]))
+    ds.add_points((5,6))
+    # assert np.all(ds.points == np.array([[1,3,5],[2,4,6]]))
+    ds.add_points([[7],[8]])
+    # assert np.all(ds.points == np.array([[1,3,5,7],[2,4,6,8]]))
+    ds.add_points(y=10,x=9)
+    # assert np.all(ds.points == np.array([[1,3,5,7,9],[2,4,6,8,10]]))
+    ds.add_points([(11),(12)])
+    # assert np.all(ds.points == np.array([[1,3,5,7,9,11],[2,4,6,8,10,12]]))
+    ds.add_points(np.array([[13],[14]]))
+    # assert np.all(ds.points == np.array([[1,3,5,7,9,11,13],[2,4,6,8,10,12,14]]))
 
-    arr.add_points(data)
-    assert np.all(arr['x'] == [1.,4.,7,10.,13])
-    assert np.all(arr['y'] == [2.,5,8,11.,14])
-    assert np.all(arr['z'] == [3,6,9,12,15])
+    assert np.all(ds['x'] == [1,3,5,7,9,11,13])
+    assert np.all(ds['y'] == [2,4,6,8,10,12,14])
+    assert np.all(ds[:,0] == [1,2])
+    assert np.all(ds[:,1] == [3,4])
+    assert np.all(ds[:,2] == [5,6])
+    assert np.all(ds[:,3] == [7,8])
+    assert np.all(ds[:,4] == [9,10])
+    assert np.all(ds[:,5] == [11,12])
+    assert np.all(ds[:,6] == [13,14])
 
-    arr.add_points(data2)
-    assert np.all(arr['x'] == [1.,4.,7,10.,13,16])
-    assert np.all(arr['y'] == [2.,5,8,11.,14,17])
-    assert np.all(arr['z'] == [3,6,9,12,15,18])
+
+def test_extend_points():
+    logging.debug('---------------Begin test_extend_points()')
+
+    ds = DataStream()
+    add_point_tester(ds)
+    check_post_add_point(ds)
+
+    ds = DataStream()
+    ds.add_points([1,3], [2,4])
+    # assert np.all(ds.points == np.array([[1,3],[2,4]]))
+    ds.add_points({'x':[5,7], 'y':[6,8]})
+    # assert np.all(ds.points == np.array([[1,3,5,7],[2,4,6,8]]))
+    ds.add_points(y=[10,12], x=[9,11])
+    # assert np.all(ds.points == np.array([[1,3,5,7,9,11],[2,4,6,8,10,12]]))
+    ds.add_points(np.array([[13,15],[14,16]]))
+    # assert np.all(ds.points == np.array([[1,3,5,7,9,11,13,15],[2,4,6,8,10,12,14,16]]))
+
+    assert np.all(ds['x'] == [1,3,5,7,9,11,13,15])
+    assert np.all(ds['y'] == [2,4,6,8,10,12,14,16])
+    assert np.all(ds[:,0] == [1,2])
+    assert np.all(ds[:,1] == [3,4])
+    assert np.all(ds[:,2] == [5,6])
+    assert np.all(ds[:,3] == [7,8])
+    assert np.all(ds[:,4] == [9,10])
+    assert np.all(ds[:,5] == [11,12])
+    assert np.all(ds[:,6] == [13,14])
+    assert np.all(ds[:,7] == [15,16])
 
 
 def test_add_point_error():
     logging.debug('---------------Begin test_add_point_error()')
     ds = DataStream()
-    addpointstester(ds)
+    add_point_tester(ds)
 
+    logging.debug('---------------Start exceptions')
     with pytest.raises(ValueError):
         try:
-            ds.add_point((0,1))
+            ds.add_points((0, 1))
         except Exception as e:
             logging.debug(e)
             raise
     with pytest.raises(ValueError):
         try:
-            ds.add_point(0, 1)
+            ds.add_points(0, 1)
         except Exception as e:
             logging.debug(e)
             raise
     with pytest.raises(ValueError):
         try:
-            ds.add_point(x=0, y=1)
+            ds.add_points(x=0, y=1)
         except Exception as e:
             logging.debug(e)
             raise
     with pytest.raises(ValueError):
         try:
-            ds.add_point({'y':99, 'z':10})
+            ds.add_points({'y':99, 'z':10})
+        except Exception as e:
+            logging.debug(e)
+            raise
+    with pytest.raises(ValueError):
+        try:
+            ds.add_points((0, 1, 2, 3))
+        except Exception as e:
+            logging.debug(e)
+            raise
+    with pytest.raises(ValueError):
+        try:
+            ds.add_points(0, 1, 2, 3)
+        except Exception as e:
+            logging.debug(e)
+            raise
+    with pytest.raises(ValueError):
+        try:
+            ds.add_points(x=0, y=1, z=2, w=3)
+        except Exception as e:
+            logging.debug(e)
+            raise
+    with pytest.raises(ValueError):
+        try:
+            ds.add_points({'x':0, 'y':99, 'z':10, 'w':1})
         except Exception as e:
             logging.debug(e)
             raise
@@ -176,11 +243,11 @@ def test_record_file_csv():
 
     ds = DataStream(record_to_file=file)
 
-    ds.add_point(dict2)
-    assert (os.linesep).join(['x,y', '1.0,1.0'])+os.linesep == open(file, 'r').read()
+    ds.add_points(dict2)
+    assert open(file, 'r').read() == (os.linesep).join(['x,y', '1.0,1.0'])+os.linesep
 
-    ds.add_point(dict2)
-    assert (os.linesep).join(['x,y', '1.0,1.0', '1.0,1.0'])+os.linesep == open(file, 'r').read()
+    ds.add_points(dict2)
+    assert open(file, 'r').read() == (os.linesep).join(['x,y', '1.0,1.0', '1.0,1.0'])+os.linesep
 
 
 def test_record_file_dict():
@@ -193,10 +260,10 @@ def test_record_file_dict():
     dict2 = {'x': 1.0, 'y': 1.0}
 
     ds = DataStream(record_to_file=file, file_format='dict')
-    ds.add_point(dict2)
+    ds.add_points(dict2)
     assert str({'x':1.0,'y':1.0}) + os.linesep == open(file, 'r').readlines()[-1]
 
-    ds.add_point(dict2)
+    ds.add_points(dict2)
     assert str({'x':1.0,'y':1.0}) + os.linesep == open(file, 'r').readlines()[-1]
 
 
@@ -210,42 +277,94 @@ def test_record_file_list():
     dict2 = {'x': 1.0, 'y': 1.0}
 
     ds = DataStream(record_to_file=file, file_format='list')
-    ds.add_point(dict2)
+    ds.add_points(dict2)
     assert 'x,y' + os.linesep + str(list(dict2.values())) + os.linesep == open(file, 'r').read()
     assert str(list(dict2.values())) + os.linesep == open(file, 'r').readlines()[-1]
 
-    ds.add_point(dict2)
+    ds.add_points(dict2)
     assert str(list(dict2.values())) + os.linesep == open(file, 'r').readlines()[-1]
 
 
-def test_array_ops():
+def test_array_read_ops():
     Dict = {'x': [1., 4.], 'y': [2., 5], 'z': [3, 6]}
-    arr = DataStream(Dict)
-    assert arr.dtype == np.float64
+    ds = DataStream(Dict)
 
     # check by point
-    assert np.all(arr[:,0] == [1., 2., 3])
-    assert np.all(arr[:,1] == [4., 5., 6])
+    assert np.all(ds[:,0] == [1., 2., 3])
+    assert np.all(ds[:,1] == [4., 5., 6])
 
     # check x
-    assert np.all(arr['x'] == arr['x',:])
-    assert np.all(arr['x'] == arr[0,:])
-    assert np.all(arr['x'] == [1,4])
-    assert arr['x',0] == 1 == arr['x'][0]
-    assert arr['x',1] == 4 == arr['x'][1]
+    assert np.all(ds['x'] == ds['x',:])
+    assert np.all(ds['x'] == ds[0,:])
+    assert np.all(ds['x'] == [1,4])
+    assert ds['x',0] == 1 == ds['x'][0]
+    assert ds['x',1] == 4 == ds['x'][1]
 
     # check y
-    assert np.all(arr['y'] == arr['y',:])
-    assert np.all(arr['y'] == arr[1,:])
-    assert np.all(arr['y'] == [2, 5])
-    assert arr['y',0] == 2 == arr['y'][0]
-    assert arr['y',1] == 5 == arr['y'][1]
+    assert np.all(ds['y'] == ds['y',:])
+    assert np.all(ds['y'] == ds[1,:])
+    assert np.all(ds['y'] == [2, 5])
+    assert ds['y',0] == 2 == ds['y'][0]
+    assert ds['y',1] == 5 == ds['y'][1]
 
     #check z
-    assert np.all(arr['z'] == arr['z',:])
-    assert np.all(arr['z'] == arr[2,:])
-    assert np.all(arr['z'] == [3, 6])
-    assert arr['z',0] == 3 == arr['z'][0]
-    assert arr['z',1] == 6 == arr['z'][1]
+    assert np.all(ds['z'] == ds['z',:])
+    assert np.all(ds['z'] == ds[2,:])
+    assert np.all(ds['z'] == [3, 6])
+    assert ds['z',0] == 3 == ds['z'][0]
+    assert ds['z',1] == 6 == ds['z'][1]
 
-    assert check_datastreams_equal(eval(repr(arr), {}, {'DataStream':DataStream,'array':np.array, 'float64':np.float64}), arr)
+    with pytest.raises(IndexError):
+        print(ds[0,'z'])
+    with pytest.raises(IndexError):
+        print(ds[:,'x'])
+
+    check_datastreams_equal(eval(repr(ds), {}, {'DataStream':DataStream, 'array':np.array}), ds)
+
+
+def test_array_set_ops():
+    Dict = {'x': [1., 4.], 'y': [2., 5], 'z': [3, 6]}
+    ds_constructed = DataStream(Dict)
+
+    # [key] =
+    ds = DataStream({'x':[0,0],'y':[0,0],'z':[0,0]})
+    ds['x'] = Dict['x']
+    assert np.all(ds_constructed['x'] == ds['x'])
+
+    ds['y'] = Dict['y']
+    assert np.all(ds_constructed['y'] == ds['y'])
+
+    assert np.all(ds_constructed.points == ds.points) == False  # 'is' doesn't want to work here
+
+    ds['z'] = Dict['z']
+    assert np.all(ds_constructed['z'] == ds['z'])
+    assert np.all(ds_constructed.points == ds.points)
+
+    # [key, :] =
+    ds = DataStream({'x':[0,0],'y':[0,0],'z':[0,0]})
+    ds['x',:] = Dict['x']
+    assert np.all(ds_constructed['x'] == ds['x'])
+
+    ds['y',:] = Dict['y']
+    assert np.all(ds_constructed['y'] == ds['y'])
+
+    assert np.all(ds_constructed.points == ds.points) == False  # 'is' doesn't want to work here
+
+    ds['z',:] = Dict['z']
+    assert np.all(ds_constructed['z'] == ds['z'])
+    assert np.all(ds_constructed.points == ds.points)
+
+    # [key][:] =
+    ds = DataStream({'x':[0,0],'y':[0,0],'z':[0,0]})
+    ds['x'][:] = Dict['x']
+    assert np.all(ds_constructed['x'] == ds['x'])
+
+    ds['y'][:] = Dict['y']
+    assert np.all(ds_constructed['y'] == ds['y'])
+
+    assert np.all(ds_constructed.points == ds.points) == False  # 'is' doesn't want to work here
+
+    ds['z'][:] = Dict['z']
+    assert np.all(ds_constructed['z'] == ds['z'])
+    assert np.all(ds_constructed.points == ds.points)
+
