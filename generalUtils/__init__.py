f240ab9ccb8ffa0162fe9ec190e7ac6b5dffc531
timjolson <tim.lsn@gmail.com>
2019-07-19 17:38:23 -0500

    Moved datastream, again

--- generalUtils/__init__.py
+++ generalUtils/__init__.py
@@ -1,20 +1,2 @@
 # methods
 from .general_utils import *
-
-# modules
-from . import color_utils
-from . import qt_utils
-from . import sympy_utils
-from . import helpers_for_tests
-from . import helpers_for_qt_tests
-from . import datastream
-from . import image_utils
-from . import differential_evolver
-
-# classes
-from .structset import StructSet
-from .stopwatch import StopWatch
-from .multiproc_handler import MultiProcHandler
-from .logger_recorder import *
-from .keyboard_detection import keyboard_detection, KeyStop
-from .myplot import MyplotWidget
