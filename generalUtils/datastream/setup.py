0a38b2374a82d92dd635d58269c8057eb208e128
timjolson <tim.lsn@gmail.com>
2019-08-08 14:41:16 -0500

    removed broken submodule

--- generalUtils/datastream/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='datastream',
-    version="0.7",
-    packages=find_packages(),
-    install_requires=['numpy'],
-    tests_require=['pytest'],
-)
